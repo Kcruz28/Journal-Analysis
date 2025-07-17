598a1a568d37f4e84ed277220b608f6d0cb9fec0
Kevin <kcruz28@illinois.edu>
2025-07-16 22:52:05 -0500

    Initial commit for Journal-Analysis project. set up spring boot

---
+++ src/main/java/com/journal/journal_analysis/JournalAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.journal.journal_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/journal/journal_analysis/JournalAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journal.journal_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
